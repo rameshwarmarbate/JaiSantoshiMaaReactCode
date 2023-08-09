af851e637ec70accaa838f2ee449e03115366816
Rameshwar <rameshwarmarbate@gmail.com>
2023-08-09 17:16:04 +0530

    bug fixes

--- frontend/src/modules/master/components/branches/BranchEdit.jsx
+++ frontend/src/modules/master/components/branches/BranchEdit.jsx
@@ -351,7 +351,7 @@ const BranchEdit = () => {
               size="medium"
               onClick={cancelButtonHandler}
             >
-              Cancel
+              Back
             </Button>
             <Button
               variant="outlined"
